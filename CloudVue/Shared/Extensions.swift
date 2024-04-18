c13b5c91772b842d20fb702c4c0671b148d264b4
Roy <95645767+RoyNkem@users.noreply.github.com>
2024-04-18 19:05:25 +0100

    Create Extensions for method to differentiate between macOS and iOS

--- CloudVue/Shared/Extensions.swift
+++ CloudVue/Shared/Extensions.swift
@@ -5,4 +5,20 @@
 //  Created by Roy's MacBook M1 on 18/04/2024.
 //
 
-import Foundation
+import SwiftUI
+
+
+// Getting screen size
+// for both mac and ios...
+
+extension View {
+    
+    func getRect() -> CGRect {
+        
+        #if os(macOS)
+        return NSScreen.main!.visibleFrame
+        #else
+        return UIScreen.main.bounds
+        #endif
+    }
+}
